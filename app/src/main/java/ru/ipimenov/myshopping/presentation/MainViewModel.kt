3ac925d7473639fee6dd1a29c3aed890fee21070
MAVERICK <bravo1@list.ru>
2022-05-21 16:55:31 +0300

    Создан presentation-слой, в нём создана MainViewModel.kt для работы с domain-слоем

---
+++ app/src/main/java/ru/ipimenov/myshopping/presentation/MainViewModel.kt
@@ -0,0 +1,36 @@
+package ru.ipimenov.myshopping.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.ipimenov.myshopping.data.ShopListRepositoryImpl
+import ru.ipimenov.myshopping.domain.DeleteShopItemUseCase
+import ru.ipimenov.myshopping.domain.EditShopItemUseCase
+import ru.ipimenov.myshopping.domain.GetShopListUseCase
+import ru.ipimenov.myshopping.domain.ShopItem
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+    fun changeEnableState(shopItem: ShopItem) {
+        val newShopItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newShopItem)
+        getShopList()
+    }
+}
